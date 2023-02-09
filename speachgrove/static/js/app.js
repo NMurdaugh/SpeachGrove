9e4f8065655fb4e3b005b4140f67c9b2e741ccec
NMurdaugh <nmurdaugh42@gmail.com>
2023-02-09 16:11:50 -0700

    fixed errors in api view and in app.js; WORKING

--- speachgrove/static/js/app.js
+++ speachgrove/static/js/app.js
@@ -9,10 +9,18 @@ new Vue({
     },
     methods: {
         speechWriter() {
-            axios.post('/api/speech').then(res => this.ttsData = res.data)
-        }
+            axios.post('/api/speech/', this.postData(), {
+                headers: { 'X-CSRFToken': this.csrfToken }
+            }).then(res => this.ttsData = res.data)
+        },
+        postData() {
+            return {
+                'speakerName': this.speakerName,
+                'userRequestText': this.userRequestText,
+         }
+         },
     },
     mounted() {
         this.csrfToken = document.querySelector('input[name=csrfmiddlewaretoken]').value
-    }
+    },
 })
